9ec02365649897f43eaefd17cdff23da33a6af91
HongBuzz <hongbuzz@gmail.com>
2018-07-15 06:54:41 +0900

    home  page added web3

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -21,9 +21,6 @@
                 <div class="col-12 col-md-9 mb-2 mb-md-0">
                   <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
                 </div>
-                <div class="col-12 col-md-3">
-                  <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-                </div>
               </div>
             </form>
           </div>
@@ -50,7 +47,7 @@
               <div class="features-icons-icon d-flex">
                 <i class="icon-layers m-auto text-primary"></i>
               </div>
-              <h3>2y</h3>
+              <h3>2</h3>
               <p class="lead mb-0">The result of matching is basically provided as % form.</p>
             </div>
           </div>
@@ -109,20 +106,118 @@
 
 <script>
 // import Vue from 'vue'
+import Web3 from 'web3'
 import router from '../router'
 export default {
   name: 'home',
   components: {
     'tastetable': () => import('./tastetable')
   },
+  mounted () {
+    this.getProvider()
+  },
   methods: {
     createTaste (tableId) {
       console.log('tableId')
       router.push({name: 'CreateTaste1', params: { tableId: tableId }})
+    },
+    getProvider () {
+      // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+      if (typeof web3 !== 'undefined') {
+        // Use Mist/MetaMask's provider
+        this.web3Provider = web3.currentProvider
+        web3 = new Web3(this.web3Provider)
+      } else {
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        this.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+        web3 = new Web3(this.web3Provider)
+      }
+      this.getContract()
+    },
+    getContract () {
+      var TutorialTokenArtifact = require('../../../build/contracts/TutorialToken.json')
+      this.contracts.TutorialToken = this.$TruffleContract(TutorialTokenArtifact)
+      this.contracts.TutorialToken.setProvider(this.web3Provider)
+      this.networkCheck()
+      this.getBalances()
+    },
+    networkCheck () {
+      web3.version.getNetwork((err, netId) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        switch (netId) {
+          case '1':
+            console.log('This is mainnet')
+            break
+          case '2':
+            console.log('This is the deprecated Morden test network.')
+            break
+          case '3':
+            console.log('This is the ropsten test network.')
+            break
+          default:
+            console.log('This is an unknown network.')
+        }
+      })
+    },
+    getBalances (adopters, account) {
+      console.log('getting balances...')
+      web3.eth.getAccounts((error, accounts) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+        this.account = accounts[0]
+        console.log(accounts)
+        this.contracts.TutorialToken.deployed().then((instance) => {
+          var tutorialTokenInstance = instance
+
+          return tutorialTokenInstance.balanceOf(this.account)
+        }).then((result) => {
+          console.log(result)
+          this.balance = result.c[0]
+        }).catch((err) => {
+          console.log(err.message)
+        })
+      })
+    },
+    makeTransfer () {
+      console.log('Transfer ' + this.toAmount + ' TT to ' + this.toAddress)
+
+      var tutorialTokenInstance
+
+      web3.eth.getAccounts((error, accounts) => {
+        if (error) {
+          console.log(error)
+          return
+        }
+
+        this.account = accounts[0]
+
+        this.contracts.TutorialToken.deployed().then((instance) => {
+          tutorialTokenInstance = instance
+
+          return tutorialTokenInstance.transfer(this.toAddress, this.toAmount, {from: this.account})
+        }).then((result) => {
+          alert('Transfer Successful!')
+          return this.getBalances()
+        }).catch((err) => {
+          console.log(err.message)
+        })
+      })
     }
   },
   data () {
     return {
+      toAddress: null,
+      toAmount: 0,
+      web3: null,
+      web3Provider: null,
+      balance: 0,
+      contracts: {},
+      account: null,
       tables: [
         {
           'text': 'Travel',
@@ -140,6 +235,7 @@ export default {
     }
   }
 }
+require('bootstrap-css-only')
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
